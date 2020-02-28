02a020acdb7c749fd36699783bd7e6c0a79f9b88
zhang5773117 <40231462+zhang5773117@users.noreply.github.com>
2020-02-28 11:19:29 +0800

    Update EsApp.java

--- myesdemo/src/main/java/com/zb/EsApp.java
+++ myesdemo/src/main/java/com/zb/EsApp.java
@@ -11,6 +11,7 @@ import java.util.List;
 @SpringBootApplication
 public class EsApp {
     //写完代码了
+    //李四提交到李四的分支上，并合并到master的分支上
     public static void main(String[] args) throws Exception {
 
         ConfigurableApplicationContext run = SpringApplication.run(EsApp.class, args);
